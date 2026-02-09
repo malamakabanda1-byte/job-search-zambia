ede376fc8cf2bac3777ddf2f715b782c4db9b31c
malamakabanda1-byte <malamakabanda1@gmail.com>
2026-02-09 16:43:06 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCZwl2Vg4J4J4WQZBRyTXL6zNhRPtDOWlc",
+  authDomain: "zambian-jobs-ce081.firebaseapp.com",
+  projectId: "zambian-jobs-ce081",
+  storageBucket: "zambian-jobs-ce081.firebasestorage.app",
+  messagingSenderId: "1022758849145",
+  appId: "1:1022758849145:web:dd571554ab04334f41e2a8",
+  measurementId: "G-KB5K9CMW60"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+</script>
